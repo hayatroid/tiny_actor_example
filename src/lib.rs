ef0799526742ef4bce000c2351380b470130b7b0
hayatroid <hayatroid@gmail.com>
2025-04-15 20:14:26 +0900

    ✨ message

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use tokio::sync::oneshot;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+type AddRequest = (i32, i32);
+type AddResponse = i32;
+type AddEnvelope = (AddRequest, oneshot::Sender<AddResponse>);
