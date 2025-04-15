1de489ed7605f15589f0abc95112abe00fa5d5d7
hayatroid <hayatroid@gmail.com>
2025-04-15 20:18:45 +0900

    ✨ actor

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
-use tokio::sync::oneshot;
+use tokio::{
+    sync::{mpsc, oneshot},
+    time::{Duration, sleep},
+};
 
 type AddRequest = (i32, i32);
 type AddResponse = i32;
 type AddEnvelope = (AddRequest, oneshot::Sender<AddResponse>);
+
+struct AddActor {
+    envelope_rx: mpsc::UnboundedReceiver<AddEnvelope>,
+}
+
+impl AddActor {
+    async fn run(&mut self) {
+        while let Some(envelope) = self.envelope_rx.recv().await {
+            let (request, response_tx) = envelope;
+            let response = self.handle(request).await;
+            response_tx.send(response).unwrap();
+        }
+    }
+    async fn handle(&self, request: AddRequest) -> AddResponse {
+        sleep(Duration::from_secs(1)).await;
+        request.0 + request.1
+    }
+}
