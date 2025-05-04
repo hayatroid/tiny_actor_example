6348ed6dc7d04a3d8ecf10c390e4ebbef7257086
hayatroid <hayatroid@gmail.com>
2025-05-04 16:20:02 +0900

    🎨 refactoring

--- examples/round_robin.rs
+++ examples/round_robin.rs
@@ -2,16 +2,18 @@ use tiny_actor_example::spawn_add_actor;
 
 #[tokio::main]
 async fn main() {
-    let add_actor_refs: Vec<_> = (0..5).map(|_| spawn_add_actor()).collect();
-    let mut futures = vec![];
-    let mut index = 0;
-    for i in 0..10 {
-        for j in 0..10 {
-            futures.push((i, j, add_actor_refs[index].send((i, j))));
-            index = (index + 1) % 5;
+    let add_actors = (0..5).map(|_| spawn_add_actor()).collect::<Vec<_>>();
+    let mut jobs = vec![];
+    let mut target = 0;
+    for x in 1..=10 {
+        for y in 1..=10 {
+            let req = (x, y);
+            let res_rx = add_actors[target].send(req);
+            jobs.push((req, res_rx));
+            target = (target + 1) % 5;
         }
     }
-    for (i, j, future) in futures {
-        println!("{} + {} = {}", i, j, future.await.unwrap());
+    for (req, res_rx) in jobs {
+        println!("{} + {} = {}", req.0, req.1, res_rx.await.unwrap());
     }
 }
